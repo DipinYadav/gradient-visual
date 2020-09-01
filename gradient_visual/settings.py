165ff08bcc8ead41bdb5b10925b46881d7a060a3
Dipin Yadav <dipin.yadav72@gmail.com>
2020-09-02 02:57:43 +0530

    changed debug to false

--- gradient_visual/settings.py
+++ gradient_visual/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('DJANGO_BLOGGIFAPP_SECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['gradientvisual.herokuapp.com']
 
