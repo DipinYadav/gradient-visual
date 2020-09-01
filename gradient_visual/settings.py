fca2b36998a7a8c5acb348a8611b7a6fb0b84b05
Dipin Yadav <dipin.yadav72@gmail.com>
2020-09-02 02:55:40 +0530

    changed settings

--- gradient_visual/settings.py
+++ gradient_visual/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('DJANGO_BLOGGIFAPP_SECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://gradientvisual.herokuapp.com']
+ALLOWED_HOSTS = ['gradientvisual.herokuapp.com']
 
 
 # Application definition
